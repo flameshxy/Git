92873bfc9efda7bc979d86e9ddc5feaefcb47b31
chihiro <2413576546@qq.com>
2022-04-07 21:19:51 +0800

    hot-file master conflict branch commit

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
         System.out.println("GitHub yyds!");
         System.out.println("第一天学Git");
         System.out.println("学习创建分支");
+        System.out.println("master 分支冲突");
     }
 }
