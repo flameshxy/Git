108f6dd9f05c53d7a78878c6b8769c29e3917f2e
chihiro <2413576546@qq.com>
2022-04-07 20:35:47 +0800

    hot-file branch commit

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("GitHub yyds!");
         System.out.println("第一天学Git");
+        System.out.println("学习创建分支");
     }
 }
